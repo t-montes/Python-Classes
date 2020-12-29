119e6357b6514f4c3094f77ecde353c0b274cc3b
Tony Montes <t.montes@uniandes.edu.co>
2020-12-29 11:50:39 -0500

    Lesson1

---
+++ Lesson1/L1_1_console.py
@@ -0,0 +1,21 @@
+""" Asignación de Variables """
+
+x = 2
+bienv = "Bienvenido!"
+
+""" Impresión en Consola (print)"""
+
+print("Hola mundo")
+print(bienv)
+
+""" Interacción con Usuarios (input) """
+
+name  = input("Digite su nombre: ")
+edad = int(input("Digite su edad: "))
+
+""" Argumentos y opciones de impresión """
+
+print(bienv, name, "usted tiene", edad, "años", end=".\n")
+print("Hasta", "Pronto", sep="-", end="!")
+
+
---
+++ Lesson1/L1_3_booleans.py
@@ -0,0 +1,83 @@
+""" Booleanos 
+
+Solo pueden tomar 2 valores:
+    True y False
+
+"""
+
+""" Operadores Lógicos """
+
+# not
+not False      #True
+not True       #False
+
+# or
+True  or True  #True
+True  or False #True
+False or True  #True
+False or False #False
+
+# and
+True  or True  #True
+True  or False #False
+False or True  #False
+False or False #False
+
+""" Operadores de comparación """
+# < (Menor)
+1  <   2   #True
+
+# > (Mayor)
+'a' > 'b'  #False
+
+# >= (Mayor o Igual)
+3  >=  3  #True
+
+# <= (Menor o Igual)
+3  <=  2  #False
+
+# == (Igual)
+3  ==  1  #False
+
+# != (Diferente)
+3  !=  4  #True
+not (3  ==  4)
+
+""" BLOQUE Condicional en cascada.
+if - elif - else """
+
+val1 = bool()
+val2 = bool()
+val3 = bool()
+
+
+if val1:
+    ...             # Si val1 == True
+elif val2:
+    ...             # Si (val1 == False) and (val2 == True)
+elif val3:
+    ...             # Si (val1 == False) and (val2 == False) and (val3 == True)
+else:
+    ...             # Si (val1 == False) and (val2 == False) and (val3 == False)
+
+
+""" Representación Numérica de Booleanos """
+
+True   == 1 
+False  == 0
+
+False - True # 0 - 1 == -1
+
+""" Operadores Identidad y Pertenencia """
+
+# in (pertenencia)
+
+'a' in "hola" # True
+'k' in "hola" # False
+
+# is (identidad)
+x1 = 0
+x2 = False
+
+x1 == x2 # True (False == 0)
+x1 is x2 # False
